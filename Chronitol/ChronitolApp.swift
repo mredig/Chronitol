a1c50e755882a912bf3ae9a5dda2e9c824b0d08a
Michael Redig <mredig@gmail.com>
2020-10-24 14:34:22 -0500

    initial commit

---
+++ Chronitol/ChronitolApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronitolApp.swift
+//  Chronitol
+//
+//  Created by Michael Redig on 10/24/20.
+//
+
+import SwiftUI
+
+@main
+struct ChronitolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
